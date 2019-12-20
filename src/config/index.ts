938841a9c6d40b1f72dd61c26e62c4de4377011f
yanlong.wang <yanlong.wang@naiver.org>
2019-12-21 01:48:54 +0800

    refactor: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,11 @@
-import { EnvConfig } from '../lib/config';
+import dev from './dev';
+import prod from './prod';
 
-export const config = new EnvConfig(__dirname, 'dev').load();
+const envMap: any = {
+    dev,
+    prod
+};
+
+export const config = envMap[process.env.NODE_ENV as any] || dev;
 
 export default config;
